6535d15e8e8cd1ef1e0f4aab6d11367bf33a50c9
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-16 15:33:40 +0700

    2_6Writing_a_Simple_Unit_Test

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumofArguments()
+        {
+            var math = new Math();
+            var result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
