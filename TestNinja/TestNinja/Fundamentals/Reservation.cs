b0589969a38edc0ed0046c952327a1fa1e43a434
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-16 15:10:27 +0700

    1_9Refectoring_with_Confidence

--- TestNinja/TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/TestNinja/Fundamentals/Reservation.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Fundamentals
+﻿namespace TestNinja.Fundamentals
 {
-    class Reservation
+    public class Reservation
     {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            return (user.IsAdmin || MadeBy == user);
+        }
+
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
     }
 }
