b4851a84ae488ade7f79f7ce9a8f2ac31b00d086
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-16 15:44:51 +0700

    2_7Black-box_Testing

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -14,5 +14,26 @@ namespace TestNinja.UnitTests
             var result = math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+            var result = math.Add(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_secondArgumentIdGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+            var result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_ArgumentAreEqual_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+            var result = math.Add(1, 1);
+            Assert.That(result, Is.EqualTo(1));
+        }
     }
 }
