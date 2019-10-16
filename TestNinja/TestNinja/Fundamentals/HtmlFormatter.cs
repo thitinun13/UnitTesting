6f2625e4a348b6983c20d4aa15f133e1bf3748f0
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-16 17:14:14 +0700

    3_5Testing_Void_Methods

--- TestNinja/TestNinja/Fundamentals/HtmlFormatter.cs
+++ TestNinja/TestNinja/Fundamentals/HtmlFormatter.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Fundamentals
+﻿namespace TestNinja.Fundamentals
 {
-    class Class1
+    public class HtmlFormatter
     {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
     }
 }
