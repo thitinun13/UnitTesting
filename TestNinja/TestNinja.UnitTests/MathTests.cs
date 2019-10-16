a60cdf39aac4f1ee7e247afdb55a134defafb27e
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-16 15:55:14 +0700

    2_8Set_Up_and_Tear_Down

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -7,32 +7,42 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        // SetUp
+        // TearDown
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumofArguments()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var result = math.Add(2, 1);
+           
+            var result = _math.Add(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_secondArgumentIdGreater_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_ArgumentAreEqual_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var result = math.Add(1, 1);
+           
+            var result = _math.Add(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
