bcd9c3489f96f567113a3fea2a30bcd012bb42e2
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-16 17:11:04 +0700

    3_5Testing_Void_Methods

--- TestNinja/TestNinja/Fundamentals/CustomerController.cs
+++ TestNinja/TestNinja/Fundamentals/CustomerController.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Fundamentals
+﻿namespace TestNinja.Fundamentals
 {
-    class Class1
+    public class CustomerController
     {
+        public ActionResult GetCustomer(int id)
+        {
+            if (id == 0)
+                return new NotFound();
+
+            return new Ok();
+        }
     }
+
+    public class ActionResult { }
+
+    public class NotFound : ActionResult { }
+
+    public class Ok : ActionResult { }
 }
