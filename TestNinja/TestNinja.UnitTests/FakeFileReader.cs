4bba9e6997e82cf1fab7ff7815e75b272aecdda9
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-17 14:22:48 +0700

    5_3Refactoring_Towards_a_Loosely-coupled_Design

---
+++ TestNinja/TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
