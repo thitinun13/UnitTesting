d029e7f4152693577e2c954a53d5eb457810699d
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-17 16:26:43 +0700

    5_11Testing_the_Interaction_Between_Two_Objects

--- TestNinja/TestNinja.UnitTests/Mocking/OrderServiceTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
-    class Class1
+    [TestFixture]
+    public class OrderServiceTests
     {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+
     }
 }
--- TestNinja/TestNinja/Mocking/OrderService.cs
+++ TestNinja/TestNinja/Mocking/OrderService.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Mocking
+﻿namespace TestNinja.Mocking
 {
-    class OrderService
+    public class OrderService
     {
+        private readonly IStorage _storage;
+
+        public OrderService(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public int PlaceOrder(Order order)
+        {
+            var orderId = _storage.Store(order);
+
+            // Some other work
+
+            return orderId;
+        }
+    }
+
+    public class Order
+    {
+    }
+
+    public interface IStorage
+    {
+        int Store(object obj);
     }
 }
