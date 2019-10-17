f8f54c3ed581b7b65ad2f15b2110f162bc99c923
thitinun13 <56579342+thitinun13@users.noreply.github.com>
2019-10-17 21:45:23 +0700

    7_2Test_Cases

---
+++ TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelper_OverlappingBookingsExistTests
+    {
+        [Test]
+        public void OverlappingBookingsExist_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
